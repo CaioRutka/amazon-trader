b4b6b02bbfda166b4ff0e1248360616c1e8894c8
rutkaca <Layla10$$>
2022-12-15 13:55:42 -0300

    initial commit

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://amazon-trader-api.herokuapp.com/' 
-: 'https://amazon-trader-api.herokuapp.com/';
+? 'https://amazon-trader-api.herokuapp.com' 
+: 'https://amazon-trader-api.herokuapp.com';
 
 export default baseUrl;
